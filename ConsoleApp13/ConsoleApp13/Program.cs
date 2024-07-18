a66b39021a3d26ed0059db8a03415cd084287488
Ivan980675 <141941222+Ivan980675@users.noreply.github.com>
2024-07-18 17:28:54 +0500

    Add files via upload

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,27 @@
+﻿public class FolderSize
+{
+    public static long GetFolderSize(string folderPath)
+    {
+        long totalSize = 0;
+
+        // Получаем список всех файлов и папок в указанной папке
+        var files = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+        var directories = Directory.GetDirectories(folderPath, "*", SearchOption.AllDirectories);
+
+        // Суммируем размер всех файлов
+        foreach (var file in files)
+        {
+            var fileInfo = new FileInfo(file);
+            totalSize += fileInfo.Length;
+        }
+
+        // Рекурсивно считаем размер вложенных папок
+        foreach (var dir in directories)
+        {
+            totalSize += GetFolderSize(dir);
+        }
+
+        return totalSize;
+    }
+ 
+}
